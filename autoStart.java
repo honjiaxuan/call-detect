52a352d8cbc9fee269e704d177803d2d7b32dab5
honjiaxuan <honjiaxuan@users.noreply.github.com>
2017-02-21 15:14:08 +0800

    Broadcast Receiver to detect call

---
+++ autoStart.java
@@ -0,0 +1,45 @@
+package com.call.detect;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.telephony.TelephonyManager;
+import android.text.format.DateFormat;
+import android.util.Log;
+
+
+public class autoStart extends BroadcastReceiver {
+    public autoStart() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        
+        
+// check is it the correct intent action, outgoing call or phone state
+        if (intent.getAction() != null) {
+            String intentAction = intent.getAction();
+            Log.d("tag1autostart1", intent.getAction() + ".");
+            if (intentAction.equals("android.intent.action.NEW_OUTGOING_CALL")) {
+                // if the intent action is outgoing call, then extract the info from the intent, show in log
+                try { 
+                     add = intent.getExtras().getString("android.intent.extra.PHONE_NUMBER"); 
+                     OutgoingCall.startOutgoingCallService(context, add); 
+                 }catch (Exception e) { 
+                     Log.d("autostart","outgoing call intent exception occur"); 
+                 } 
+            } else if (intentAction.equals("android.intent.action.PHONE_STATE")) { 
+                 try { 
+                     String stateStr = intent.getExtras().getString(TelephonyManager.EXTRA_STATE); 
+                     String number = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER); 
+                     PhoneState.startPhoneStateService(context,stateStr,number); 
+                 } catch (Exception e) { 
+                     Log.e("tag1Phone Receive Error", " " + e); 
+                 } 
+             } 
+        }
+    }
+}
+
